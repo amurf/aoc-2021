457293fb2bdbf4f1dc89a40efb2316120b4dae86
ash <ash@strategicdata.com.au>
2021-12-07 16:24:36 +1100

    Add solution for day 1

---
+++ 1.js
@@ -0,0 +1,41 @@
+const { readInput } = require("./helpers");
+
+const input = readInput("1.input");
+const inputArray = input.split("\n");
+
+console.log(partOne());
+console.log(partTwo());
+
+function partOne() {
+  let increasingCount = 0;
+
+  for (let i = 1; i < inputArray.length - 1; i++) {
+    if (inputArray[i] > inputArray[i - 1]) {
+      increasingCount++;
+    }
+  }
+
+  return increasingCount;
+}
+
+function partTwo() {
+  let increasingCount = 0;
+
+  for (let i = 0; i < inputArray.length - 2; i++) {
+    // less efficient but clearer code
+    let previous = sumNextThree(inputArray, i);
+    let current = sumNextThree(inputArray, i + 1);
+
+    if (current > previous) {
+      increasingCount++;
+    }
+  }
+
+  return increasingCount;
+}
+
+function sumNextThree(array, index) {
+  return array
+    .slice(index, index + 3)
+    .reduce((a, b) => parseInt(a) + parseInt(b));
+}
